1e8f480b363cb8a728309a12b1ffa2a572a2bea8
Vanderson Telema <vandelson14@hotmail.com>
2021-09-11 02:47:04 +0100

    refactor: validation

--- app/Validation/UserValidation.php
+++ app/Validation/UserValidation.php
@@ -12,8 +12,8 @@ final class UserValidation
   static function validateUser()
   {
     return Validator::make(request()->all(), [
-      User::NAME => 'required|string|max:255|',
-      User::EMAIL => 'required|string|email|max:255|unique:users',
+      User::NAME => 'required|string',
+      User::EMAIL => 'required|string|email|unique:users',
       User::ROLE => ['required', Rule::in(User::ROLES)],
       User::PASSWORD => 'required|string|min:6|confirmed',
     ]);
@@ -22,14 +22,14 @@ final class UserValidation
   static function validateStudent()
   {
     return Validator::make(request()->all(), [
-      User::NAME => 'required|string|max:255|',
+      User::NAME => 'required|string',
       User::EMAIL => "required|string|email|unique:users",
-      User::UF => 'required|string|max:3|',
-      User::CPF => 'required|string|max:9',
-      User::ADDRESS => 'required|string|max:25',
-      User::COMPANY => 'required|string|max:70',
-      User::TELEPHONE => 'required|string|max:20',
-      User::PHONE => 'required|string|max:20',
+      User::UF => 'required|string|max:2|',
+      User::CPF => 'required|string|max:11',
+      User::ADDRESS => 'required|string',
+      User::COMPANY => 'required|string',
+      User::TELEPHONE => 'required|string',
+      User::PHONE => 'required|string',
       User::CATEGORY => ['required', Rule::in(User::CATEGORIES)],
       User::ROLE => ['required', Rule::in(User::ROLES)],
       User::PASSWORD => 'required|string|min:6|confirmed',
@@ -39,14 +39,14 @@ final class UserValidation
   static function validateStudentOnUpdate(int $user_id = null)
   {
     return Validator::make(request()->all(), [
-      User::NAME => 'required|string|max:255|',
+      User::NAME => 'required|string',
       User::EMAIL => "required|string|email|unique:users,email,{$user_id},id",
-      User::UF => 'required|string|max:3|',
-      User::CPF => 'required|string|max:9',
-      User::ADDRESS => 'required|string|max:25',
-      User::COMPANY => 'required|string|max:70',
-      User::TELEPHONE => 'required|string|max:20',
-      User::PHONE => 'required|string|max:20',
+      User::UF => 'required|string|max:1|',
+      User::CPF => 'required|string|max:11',
+      User::ADDRESS => 'required|string',
+      User::COMPANY => 'required|string',
+      User::TELEPHONE => 'required|string',
+      User::PHONE => 'required|string',
       User::CATEGORY => ['required', Rule::in(User::CATEGORIES)],
       User::ROLE => ['required', Rule::in(User::ROLES)]
     ]);
